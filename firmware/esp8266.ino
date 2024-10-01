379bc0e7d8dc9d66c84663838c2ec8616972e8c6
Vladimir Rodkin <mail@vovanr.com>
2024-10-01 11:44:12 +0300

    feat: update firmware to led Camera and Microphone status

--- firmware/esp8266.ino
+++ firmware/esp8266.ino
@@ -15,72 +15,86 @@ WiFiClient wifiClient;
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
 
-// Switch on builtin Led when camera is active
 #define LED_BUILTIN D4
 
+uint8_t CameraLedPin = D1;
+uint8_t MicrophoneLedPin = D2;
+
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    // Initialize the LED_BUILTIN pin as an output
-    pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(CameraLedPin, OUTPUT);
+  pinMode(MicrophoneLedPin, OUTPUT);
 
-    WiFi.begin(ssid, password);
-    Serial.println("Connecting");
-    while(WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("");
-    Serial.print("Connected to WiFi network with IP Address: ");
-    Serial.println(WiFi.localIP());
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 
-    Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
 void loop() {
-    // Send an HTTP POST request every 5 seconds
-    if ((millis() - lastTime) > timerDelay) {
-        // Check WiFi connection status
-        if (WiFi.status() == WL_CONNECTED) {
-            HTTPClient http;
-            boolean isLedOn = false;
+  // Send an HTTP POST request every 5 seconds
+  if ((millis() - lastTime) > timerDelay) {
+    // Check WiFi connection status
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+      boolean isLedOn = false;
 
-            // API path to get status
-            String apiPath = serverAddress + "/api/status";
-            http.begin(wifiClient, apiPath.c_str());
-            // http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+      // API path to get status
+      String apiPath = serverAddress + "/api/status";
+      http.begin(wifiClient, apiPath.c_str());
+      // http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
 
-            // Send HTTP GET request
-            int httpResponseCode = http.GET();
-            if (httpResponseCode > 0) {
-                Serial.print("HTTP Response code: ");
-                Serial.println(httpResponseCode);
-                String payload = http.getString();
-                Serial.println(payload);
-                // TODO: Use JSON parser to read "status" value
-                if (
-                        payload == "{\"status\":\"ok\",\"responseData\":{\"camera\":true,\"microphone\":true}}" ||
-                        payload == "{\"status\":\"ok\",\"responseData\":{\"camera\":true,\"microphone\":false}}" ||
-                        payload == "{\"status\":\"ok\",\"responseData\":{\"camera\":false,\"microphone\":true}}"
-                   ) {
-                    isLedOn = true;
-                    // Turn the LED on (builtin led voltage level logic is inverted)
-                    digitalWrite(LED_BUILTINN, LOW);
-                } else {
-                    // Turn the LED off
-                    digitalWrite(LED_BUILTINN, HIGH);
-                }
-            } else {
-                Serial.print("Error code: ");
-                Serial.println(httpResponseCode);
-                // Turn the LED off
-                digitalWrite(LED_BUILTINN, HIGH);
-            }
-            // Free resources
-            http.end();
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+        // TODO: Use JSON parser to read "status" value
+        if (payload == "{\"status\":\"ok\",\"responseData\":{\"camera\":true,\"microphone\":true}}") {
+          setLedStatus(HIGH, HIGH);
+        } else if (payload == "{\"status\":\"ok\",\"responseData\":{\"camera\":true,\"microphone\":false}}") {
+          setLedStatus(HIGH, LOW);
+        } else if (payload == "{\"status\":\"ok\",\"responseData\":{\"camera\":false,\"microphone\":true}}") {
+          setLedStatus(LOW, HIGH);
         } else {
-            Serial.println("WiFi Disconnected");
+          setLedStatus(LOW, LOW);
         }
-        lastTime = millis();
+        toggleBuiltinLed(false);
+      } else {
+        toggleBuiltinLed(true);
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    } else {
+      Serial.println("WiFi Disconnected");
     }
+    lastTime = millis();
+  }
+}
+
+void setLedStatus(bool camera, bool microphone) {
+  digitalWrite(CameraLedPin, camera);
+  digitalWrite(MicrophoneLedPin, microphone);
+}
+
+void toggleBuiltinLed(bool value) {
+  // Builtin LED level is inverted: LOW - switch on, HIGH - switch off
+  if (value == true) {
+    digitalWrite(LED_BUILTIN, LOW);
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
