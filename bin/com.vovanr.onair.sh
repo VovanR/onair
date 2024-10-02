18a8d915bfef5d869f2046b1d7e3110df283f9f0
Vladimir Rodkin <mail@vovanr.com>
2024-10-02 17:33:44 +0300

    fix:  status only for default microphone

--- bin/com.vovanr.onair.sh
+++ bin/com.vovanr.onair.sh
@@ -4,7 +4,8 @@ SERVER_ADDRESS="http://192.168.1.1:3000"
 
 fn () {
     cameraActive=$(lsmod | grep uvcvideo --max-count=1 | awk '{print $3}' | grep -qi "0" && echo "false" || echo "true")
-    microphoneActive=$(pactl list sources | grep -qi "Mute: yes" && echo "false" || echo "true")
+    activeMicrophone=$(pactl info | grep "Default Source" | awk '{print $3}')
+    microphoneActive=$(pactl list | grep -A 10 "Name: ${activeMicrophone}" | grep -qi "Mute: yes" && echo "false" || echo "true")
     if [ ${cameraActive} == "true" ] || [ ${microphoneActive} == "true" ]; then
         curl -d $(echo "{\"camera\":${cameraActive},\"microphone\":${microphoneActive}}") -X POST ${SERVER_ADDRESS}/api/status -H "Content-Type: application/json"
     fi
